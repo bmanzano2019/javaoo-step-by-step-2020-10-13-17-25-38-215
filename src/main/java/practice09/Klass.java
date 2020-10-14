0c2e298c8ca57d53c9d8f9ededc46b577d8fc45d
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-14 22:45:05 +0800

    [OOP][Baron] Feature - Practice 9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students;
+
+    public Klass(int number) {
+        this.number = number;
+        students = new ArrayList<Student>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (students.contains(student)) {
+            leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,30 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String greeting = super.introduce() + " I am a Student. ";
+
+        if (klass.getLeader() == null) {
+            greeting += "I am at Class " + klass.getNumber() + ".";
+        }
+        else if (klass.getLeader().getId() == super.getId()) {
+            greeting += "I am Leader of Class " + klass.getNumber() + ".";
+        } else {
+            greeting += "I am at Class " + klass.getNumber() + ".";
+        }
+
+        return greeting;
+    }
 }
