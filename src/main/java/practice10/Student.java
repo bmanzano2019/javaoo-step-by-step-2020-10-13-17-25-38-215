9cd22ab0bcb15317dde4d6ba112858ac7473f699
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-14 23:34:31 +0800

    [OOP][Baron] Feature - Practice 10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,31 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+        klass.appendMember(this);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String greeting = super.introduce() + " I am a Student. ";
+
+        if (klass.getLeader() == null) {
+            greeting += "I am at Class " + klass.getNumber() + ".";
+        }
+        else if (klass.getLeader().getId() == super.getId()) {
+            greeting += "I am Leader of Class " + klass.getNumber() + ".";
+        } else {
+            greeting += "I am at Class " + klass.getNumber() + ".";
+        }
+
+        return greeting;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,59 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        classes = new LinkedList<>();
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        String greeting = super.introduce() + " I am a Teacher. ";
+
+        if (classes.size() != 0) {
+            greeting += "I teach Class "
+                    + classes.stream().map(klass -> klass.getNumber().toString())
+                    .reduce("", (result, classNum) -> {
+                        if (result.equals("")) {
+                            return classNum;
+                        } else {
+                            return result + (", " + classNum);
+                        }
+                    })
+                    + ".";
+        } else {
+            greeting += "I teach No Class.";
+        }
+
+        return greeting;
+    }
+
+    public String introduceWith(Student student) {
+        String greeting = super.introduce() + " I am a Teacher. ";
+
+        if (isTeaching(student)) {
+            greeting += "I teach " + student.getName() + ".";
+        } else {
+            greeting += "I don't teach " + student.getName() + ".";
+        }
+
+        return greeting;
+    }
+
+    public boolean isTeaching(Student student) {
+        return classes.stream().anyMatch(klass -> klass.isIn(student));
+    }
 }
