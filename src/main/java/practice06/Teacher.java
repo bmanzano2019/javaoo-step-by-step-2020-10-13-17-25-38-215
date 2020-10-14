c8ca21a526add9eac3e7f1897ba2a2f187a5ecf5
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-14 17:19:18 +0800

    [OOP][Baron] Feature - Practice 6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String greeting = super.introduce() + " I am a Teacher. ";
+
+        if (klass != null) {
+            greeting += "I teach Class " + klass + ".";
+        } else {
+            greeting += "I teach No Class.";
+        }
+
+        return greeting;
+    }
 }
